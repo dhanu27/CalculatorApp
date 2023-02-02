47a00e3e150c57f7efe8c428f7fcdd2d466fc479
DhanuBhardwaj09 <dhanu.bhardwaj@zestmoney.in>
2023-02-02 22:48:27 +0530

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,20 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Dhanu Bhardwaj on 02/02/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
